9d12666453ba289e33f3571dfd5c70f2b0e44df8
Guilherme Nunes <guilhermewnunes@gmail.com>
2019-02-09 11:39:06 +0000

    Enviroment Variables

--- src/common/config-api.js
+++ src/common/config-api.js
@@ -1,7 +1,7 @@
 import axios  from 'axios'
 
 const client = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: process.env.REACT_APP_SERVER_ADDRESS              
 });
 
 const request = function(options) {
